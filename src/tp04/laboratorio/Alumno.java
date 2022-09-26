dacb1249c52dfbdb15574c8e2374fe1432eb5e9a
pablojlpk <113646557+pablojlpk@users.noreply.github.com>
2022-09-26 13:12:15 -0300

    pablo
    tp04-comienzo

---
+++ src/tp04/laboratorio/Alumno.java
@@ -0,0 +1,48 @@
+package tp04.laboratorio;
+
+import java.util.HashSet;
+
+public class Alumno {
+
+    private int legajo;
+
+    private String apellido;
+
+    private String nombre;
+
+    private HashSet <Materia> mat = new HashSet();
+
+    public Alumno(int legajo, String apellido, String nombre) {
+    }
+
+    public int getLegajo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int setLegajo(int legajo) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getApellido() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String setApellido(String apellido) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String setNombre(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void agregarMateria(Materia m) {
+    }
+
+    public int CantidadMaterias() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/tp04/laboratorio/Materia.java
@@ -0,0 +1,37 @@
+package tp04.laboratorio;
+
+public class Materia {
+
+    private int idMateria;
+
+    private String Nombre;
+
+    private int anio;
+
+    public Materia(int idMateria, String nombre, int anio) {
+    }
+
+    public void Materia(int idMateria, String nombre, int anio) {
+    }
+
+    public int getIdMateria() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setIdMateria(int idMateria) {
+    }
+
+    public String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setNombre(String nombre) {
+    }
+
+    public int getAnio() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setAnio(int anio) {
+    }
+}
