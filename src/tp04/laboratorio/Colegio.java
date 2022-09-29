fad90b3dd6edc05c04320224a58cf7704a7f4152
pablojlpk <113646557+pablojlpk@users.noreply.github.com>
2022-09-29 20:53:13 -0300

    Update Colegio.java

--- src/tp04/laboratorio/Colegio.java
+++ src/tp04/laboratorio/Colegio.java
@@ -12,7 +12,18 @@ import java.util.HashSet;
  * @author sistema
  */
 public class Colegio {
-      HashSet <Materia> lmateria=new HashSet();
+    
+    Materia mat1=new Materia(1,"INGLES PRIMER AÑO",1);
+    Materia mat2=new Materia(2,"MATEMATICA PRIMER AÑO",1);
+    Materia mat3=new Materia(3,"LABORATORIO 1 PRIMER AÑO",1);
+    
+ 
+    Alumno al1 =new Alumno (1001,"López Martín","1001");
+    Alumno al2=new Alumno (1002,"Martinez Brenda","1002");
+    
+    
+    
+    HashSet <Materia> lmateria=new HashSet();
       
 
       
